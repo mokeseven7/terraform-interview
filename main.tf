5f6031727ac71d0f77870bd35733b79dba58e84f
Mike McGrath <mokeseven7@gmail.com>
2021-09-20 18:11:01 +0000

    tags

--- main.tf
+++ main.tf
@@ -36,20 +36,16 @@ module "user" {
 }
 
 resource "aws_cloud9_environment_ec2" "environment" {
-  count                       = var.count
   name                        = var.name
   instance_type               = var.instance_type
   automatic_stop_time_minutes = var.automatic_stop_time_minutes
   description                 = var.description
   owner_arn                   = var.owner_arn
   subnet_id                   = var.subnet_id
-  tags = merge(
-    {
-      "NameCount" = var.enviroment_count > 1 || var.use_num_suffix ? format("%s-%d", var.name, 
-        count.index + 1) : var.name
-    },
-    var.tags,
-  )
+  tags = {
+      Environment = "Test"
+      Name        = "Provider Tag"
+    }
 }
 
 
